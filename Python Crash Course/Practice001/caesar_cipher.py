67486c044597f760f3947348ca25d7b464422798
James Konomanyi <55304225+JaymsKay007@users.noreply.github.com>
2022-12-16 19:36:52 +0000

    Update caesar_cipher.py

--- Python Crash Course/Practice001/caesar_cipher.py
+++ Python Crash Course/Practice001/caesar_cipher.py
@@ -1,4 +1,5 @@
 """
+###----------------------ENCRYPT CODE INSTRUCTION-------------------
 TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount
 and print the encrypted text.
@@ -15,7 +16,7 @@ and print the encrypted text.
 TODO-3: Call the encrypt function and pass in the user inputs.
 #  You should be able to test the code and encrypt a message.
 
-###----------------------DECRYPT TEXT-------------------
+###----------------------DECRYPT CODE INSTRUCTION-------------------
 TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
   #e.g.
